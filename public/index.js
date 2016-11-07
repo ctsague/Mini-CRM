0ed0787dfe5766256508026ff5f95c0577357146
tsague <tsagueymele@yahoo.fr>
2016-11-07 15:22:52 +0100

    Etape 1

---
+++ public/index.js
@@ -0,0 +1,46 @@
+(function(){
+"use strict";
+
+var crm = {
+
+	init :function(){
+		
+		this.insererInfoDsHtml();
+	},
+
+	insererInfoDsHtml:function(){
+
+		$.ajax("/crm.json")
+		.done(this.insertDone)
+		.fail(this.insertFail);
+
+	},
+
+	insertDone : function(data){
+		console.log(data);
+		var len = data.customers.length;
+		for (var i = 0; i <=len; i++){
+			var obj = data.customers[i];
+			for(var prop in obj){
+				var individu = prop + " : " + obj[prop];
+	  		//	$("#insert").append('<ul><li>' + individu +'</li></ul>');
+	  			}
+	  		}	
+		},
+
+	insertFail : function(){
+		console.log("insertFail");
+
+	},
+	
+
+
+
+};
+
+
+$(document).ready(function(){
+	crm.init();
+});
+
+}) ();
